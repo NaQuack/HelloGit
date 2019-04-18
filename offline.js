b4d39e680e9124c473a8843d667c1e8582550e7c
NaQuack <49745021+NaQuack@users.noreply.github.com>
2019-04-18 14:06:42 +0700

    First commit

---
+++ offline.js
@@ -0,0 +1,16 @@
+﻿{
+	"version": 1555482183,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bhello-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
